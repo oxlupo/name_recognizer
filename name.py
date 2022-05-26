4cc46b0925067290dc9cca83ba116a41e2f57c90
yousef <mr.ysaj@gmail.com>
2022-05-26 09:59:04 +0430

    [UPDATE] add dataframe to name.py

--- name.py
+++ name.py
@@ -1,6 +1,14 @@
 import names_dataset
+import pandas as pd
+import logging
+from simpletransformers.classification import ClassificationModel, ClassificationArgs
 
+logging.basicConfig(level=logging.INFO)
+transformers_logger = logging.getLogger("transformers")
+transformers_logger.setLevel(logging.WARNING)
 name = names_dataset.NameDataset()
+
+
 first_name = name.first_names
 last_name = name.last_names
 
@@ -14,5 +22,12 @@ for key, value in first_name.items():
 for key, value in last_name.items():
     first_name_list.append([key, 1])
 
-train_list = first_name_list + last_name_list
+train_data = first_name_list[:len(first_name_list)//2] + last_name_list[:len(last_name_list)//2]
+train_df = pd.DataFrame(train_data)
+train_df.columns = ["text", "labels"]
 
+eval_data = first_name_list[len(first_name_list)//2:] + last_name_list[len(last_name_list)//2:]
+eval_df = pd.DataFrame(eval_data)
+eval_df.columns = ["text", "labels"]
+
+model_args = ClassificationArgs(num_train_epochs=10)
